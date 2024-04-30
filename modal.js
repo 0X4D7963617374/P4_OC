3c888809ae73f34d1a9f3b401e4c56381f77fc54
0X4D7963617374 <olivier@helioscast.com>
2024-04-30 23:21:51 +0200

    [feat]validate: base du code

--- modal.js
+++ modal.js
@@ -1,23 +1,128 @@
-function editNav() {
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelector(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+
+const my_close = document.querySelector(".close");
+
+
+function first()
+{
+  return true;
+}
+
+function last()
+{
+  return true;
+}
+
+function email() 
+{
+  return true;
+}
+
+function quantity() 
+{
+  return true;
+}
+
+function birthdate()
+{
+  return true;
+}
+
+function localisation()
+{
+  return true;
+}
+
+function check()
+{
+  return true;
+}
+
+function tout_ok()
+{
+
+}
+
+const validators = {
+  first: first,
+  last: last,
+  email: email,
+  birthdate: birthdate,
+  localisation: localisation,
+  check: check,
+  quantity: quantity
+};
+
+/*
+// en fonction du choix de mon mentor
+function validate()
+{
+  if (validateRecursive(0, true))
+    tout_ok();
+}
+
+function validateRecursive(index, flag_valid)
+{
+  if (index >= formData.length)
+    return flag_valid;
+
+  const element = formData[index].querySelector('input');
+  return validateRecursive(++index, flag_valid && (!element || !validators[element.name] || validators[element.name]()));
+}*/
+
+
+function validate()
+{
+  let index;
+  let element;
+  let flag_valid;
+
+  flag_valid = true;
+  index = 0;
+  while (index < formData.length)
+  {
+    element = formData[index++].querySelector('input');
+    if (element && validators[element.name])
+      flag_valid = flag_valid && validators[element.name]();
+  }
+  if (flag_valid)
+    tout_ok();
+}
+
+
+
+// launch modal form
+function launchModal()
+{
+  modalbg.style.display = "block";
+}
+
+function fonct_close()
+{
+  modalbg.style.display = "none";
+}
+
+function editNav()
+{
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  if (x.className === "topnav")
+  {
     x.className += " responsive";
-  } else {
+  } else
+  {
     x.className = "topnav";
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+function init()
+{
+  if (modalBtn)
+    modalBtn.addEventListener("click", launchModal);
+  if (my_close)
+    my_close.addEventListener("click", fonct_close);
 }
 
-
+init();
